6c877cce6e2a4c92df80f8965b5f938685876c5e
goatmeat <116953860+goatmeat3@users.noreply.github.com>
2024-10-04 11:35:09 +0500

    п

--- reading_file.py
+++ reading_file.py
@@ -1,52 +1,37 @@
-from jsonschema import validate
-import jsonschema
 import json
+from jsonschema import validate, ValidationError
 from word_processing import process_word
 
 def read_word_from_json(filename):
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            data = json.load(file)
 
-  try:
+            # Проверка валидности структуры JSON
+            schema = {
+                "type": "array",
+                "items": {"type": "string"},
+                "minItems": 1
+            }
 
-    with open(filename, 'r', encoding='utf-8') as file:
+            validate(instance=data, schema=schema)
 
-      data = json.load(file)
+            word_count = {}
+            for word in data:
+                processed_word = process_word(word)
+                if processed_word:
+                    if processed_word not in word_count:
+                        word_count[processed_word] = 1
+                    else:
+                        word_count[processed_word] += 1
 
-      try:
-        schema = {
-          "type": "array",
-          "items": {
-            "type": "string"
-          },
-          "minItems": 1  # В файле должно быть > 1 элемента
-        }
+            return word_count
 
-        validate(instance=data, schema=schema)
-
-        dict = {}
-
-        for i in data:
-          istina = process_word(i)
-          if istina and ' ' in istina:
-            for index, word in enumerate(istina.split()):
-              if word not in dict:
-                dict[word] = 1
-              else:
-                dict[word] +=1
-
-          if istina and ' ' not in istina:
-            if istina not in dict:
-              dict[istina] = 1
-            else:
-              dict[istina] += 1
-
-        return dict
-
-      except jsonschema.exceptions.ValidationError as e:
-        return print(f"Ошибка валидации JSON: {e}")
-
-  except FileNotFoundError:
-    return print(f"Ошибка: возможно файл поврежден.\nУбедитесь что ваш файл "
-          f"формата JSON и соответствует виду, приведенному в инструкции")
-
-
-
+    except FileNotFoundError:
+        print(f"Ошибка: Файл не найден.")
+    except ValidationError as e:
+        print(f"Ошибка валидации JSON: {e}")
+    except json.JSONDecodeError:
+        print("Ошибка чтения JSON файла.")
+    except Exception as e:
+        print(f"Неизвестная ошибка: {e}")
--- word_processing.py
+++ word_processing.py
@@ -1,29 +1,21 @@
 import re
 import string
 import pymorphy3
-import nltk
 from nltk.corpus import wordnet
-from nltk.stem import WordNetLemmatizer
 from deep_translator import GoogleTranslator
 from pathlib import Path
 
-#nltk.download('stopwords')
-#nltk.download('wordnet')
-
-# Путь к файлу со списком матерных слов
 mat_words_path = Path("data/mat.txt")
 
 with open(mat_words_path, "r", encoding="utf-8") as f:
     mat_words_set = {line.strip() for line in f}
 
 morph = pymorphy3.MorphAnalyzer()
-lemmatizer = WordNetLemmatizer()
-
 translator = GoogleTranslator(source='auto', target='ru')
 
 def socroshalka(w1):
     if wordnet.synsets(w1):
-        w1 = translator.translate(w1, dest='ru')
+        w1 = translator.translate(w1)
     if w1 not in mat_words_set and all(len(word) > 3 for word in w1.split()):
         lemma = morph.parse(w1)[0].normal_form
         if len(lemma) > 3:
@@ -34,17 +26,7 @@ def process_word(word):
     try:
         word = word.lower().strip()
         if word.strip(string.whitespace) == word and ' ' in word:
-            if any(re.match(r'[а-яА-Я]', char) for char in word) and any(re.match(r'[a-zA-Z]', char) for char in word) :
-                word2 = ''
-                for i in word.split():
-                    word2 += socroshalka(i) + " "
-                return word2.strip()
-            else:
-                word2 = ''
-                for i in word.split():
-                    word2 += socroshalka(i) + " "
-                return word2.strip()
-
+            return ' '.join(socroshalka(i) for i in word.split())
         return socroshalka(word)
     except:
         return False
